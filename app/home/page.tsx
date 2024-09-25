beec22a32e48889ce5bb969bf0f2efbe62270750
ynurr <0305_08@naver.com>
2024-09-26 00:29:36 +0900

    refactor: CSS 모듈화

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,38 +1,39 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import PopularList from './BestList'
 import RatingList from './RatingList'
 import UserRanking from './UserRanking';
 import Recommendations from './Recommendations';
 import LatestList from './LatestList';
+import BestList from './BestList';
+import styles from './../(styles)/HomePage.module.css'
 
 export default function Main() {
   return (
-    <div className="container">
+    <div className={styles['container']}>
       <main>
         <section>
-          <h1 className="logo">
-            <span className="logo-top">LOOK</span>
-            <span className="logo-bottom">BOOK</span>
+          <h1 className={styles['logo']}>
+            <span className={styles['logo-top']}>LOOK</span>
+            <span className={styles['logo-bottom']}>BOOK</span>
           </h1>
         </section>
-        <section className="search-section">
-          <p className="sub-title">나만의 독서 경험을 기록하고<br/>다양한 독자들의 감상을 살펴보세요</p>
-          <div className="search-container">
-            <FontAwesomeIcon icon={faSearch} className="search-icon" />
-            <input className="search-input" type="text" placeholder="제목 또는 작가를 입력하세요" />
+        <section className={styles['search-section']}>
+          <p className={styles['sub-title']}>나만의 독서 경험을 기록하고<br/>다양한 독자들의 감상을 살펴보세요</p>
+          <div className={styles['search-container']}>
+            <FontAwesomeIcon icon={faSearch} className={styles['search-icon']} />
+            <input className={styles['search-input']} type="text" placeholder="제목 또는 작가를 입력하세요" />
           </div>
         </section>
-        <section className='best-book-list-section'>
-          <PopularList />
+        <section className={styles['best-book-list-section']}>
+          <BestList />
         </section>
-        <section className='rating-book-list-section'>
+        <section className={styles['rating-book-list-section']}>
           <RatingList />
         </section>
-        <section className='latest-book-list-section'>
+        <section className={styles['latest-book-list-section']}>
           <LatestList />
         </section>
-        <section className='ranking-and-recommendations-section'>
+        <section className={styles['ranking-and-recommendations-section']}>
           <div>
             <UserRanking />
           </div>
