1f50319d7b558585c487786b2dce0bdc5fdccf8c
ynurr <0305_08@naver.com>
2024-10-22 23:06:07 +0900

    refactor: Pagination 컴포넌트 폴더 이동

--- app/detail/Review.tsx
+++ app/detail/Review.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import styles from './../(styles)/Detail.module.css'
-import Pagination from './Pagination'
+import Pagination from '../(components)/Pagination'
 
 export default function Review() {
 
