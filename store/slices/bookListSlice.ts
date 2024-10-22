34df0af0317c344547916603be0fe64a376a78af
ynurr <0305_08@naver.com>
2024-10-22 23:05:20 +0900

    refactor: Pagination 컴포넌트 폴더 이동

--- store/slices/bookListSlice.ts
+++
@@ -1,9 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const listSlice = createSlice({
-    name: "books",
-    initialState: null,
-    reducers: {}
-})
-
-export default listSlice.reducer
