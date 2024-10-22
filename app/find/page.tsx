3d816dfe346b4163e443041019d944c854108882
ynurr <0305_08@naver.com>
2024-10-22 22:20:41 +0900

    feat: 검색 결과 페이지 레이아웃 추가

---
+++ app/find/page.tsx
@@ -0,0 +1,28 @@
+import styles from './Find.module.css'
+
+export default function Find() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <h2>'해리포터' 검색 결과</h2>
+                <hr className={styles.hr}/>
+                {[1,2,3,4,5].map((_, index) => (
+                    <div key={index} className={styles.inner}>
+                        <div className={styles.content}>
+                            <div className={styles.cover}>표지</div>
+                            <div className={styles.item}>
+                                <p className={styles.title}>해리 포터와 마법사의 돌</p>
+                                <p className={styles.author}>J.K. 롤링 (지은이)</p>
+                                <div className={styles.info}>
+                                    <span className={styles.publisher}>문학수첩</span>
+                                    <span className={styles.separator}>|</span>
+                                    <span className={styles.pubDate}>2019년 11월 19일</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
