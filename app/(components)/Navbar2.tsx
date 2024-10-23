57f25084a731fcbf51a6d59d5369ff7d17354e6f
ynurr <0305_08@naver.com>
2024-10-23 22:59:52 +0900

    feat: 검색 기능 추가

--- app/(components)/Navbar2.tsx
+++ app/(components)/Navbar2.tsx
@@ -1,9 +1,24 @@
+'use client'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
 import styles from './../(styles)/Navbar2.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 export default function Navbar2() {
+
+    const [keyword, setKeyword] = useState<string>('');
+    const router = useRouter();
+
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (keyword.trim()) {
+            router.push(`/find?q=${encodeURIComponent(keyword.trim())}`)
+        }
+    }
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.content}>
@@ -23,8 +38,17 @@ export default function Navbar2() {
                         </ul>
                     </div>
                     <div className={styles.rightSection}>
-                        <FontAwesomeIcon icon={faSearch} className={styles.iconSearch} />
-                        <input className={styles.search} type="text" />
+                        <form onSubmit={handleSearch}>
+                            <div className={styles.searchBox}>
+                                <FontAwesomeIcon icon={faSearch} className={styles.iconSearch} />
+                                <input 
+                                    className={styles.search} 
+                                    type="text" 
+                                    value={keyword}
+                                    onChange={(e: React. ChangeEvent<HTMLInputElement>) => setKeyword(e.target.value)}
+                                />
+                            </div>
+                        </form>
                         <Link href="/my-info" className={styles.userIconLink}>
                             <FontAwesomeIcon icon={faUser} className={styles.iconUser}/>
                         </Link>
