14be8b23392aa17e8052b0ec5ba147efb9ed2bfc
ynurr <0305_08@naver.com>
2025-01-07 20:50:31 +0900

    feat: 리뷰 작성 기능

---
+++ app/api/db/review/write/route.ts
@@ -0,0 +1,59 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+
+    if (!body.sub) {
+        return NextResponse.json({ message: "사용자 정보가 존재하지 않습니다." }, { status: 400 })
+    } else if (!body.isbn) {
+        return NextResponse.json({ message: "도서 정보가 존재하지 않습니다." }, { status: 400 });
+    } else if (!body.content) {
+        return NextResponse.json({ message: "리뷰 내용을 작성해주세요." }, { status: 400 });
+    } else if (!body.rating) {
+        return NextResponse.json({ message: "별점을 평가해주세요." }, { status: 400 });
+    }
+    
+    try {
+        const db = (await connectDB).db("lookbook")
+        
+        const result = await db.collection('reading').updateOne(
+            { _id: new ObjectId(body.sub), book_isbn: body.book_isbn },
+            {
+                $set: {
+                    content: body.content,
+                    rating: body.rating,
+                    status : 2,
+                    like_count : 0,
+                    updated_at: new Date(Date.now() + 9 * 60 * 60 * 1000)
+                },
+                $setOnInsert: {
+                    user_id: new ObjectId(body.sub),
+                    book_isbn: body.isbn,
+                    book_title: body.title,
+                    book_cover: body.cover,
+                    book_author: body.author,
+                    created_at: new Date(Date.now() + 9 * 60 * 60 * 1000),
+                },
+            },
+            { upsert: true }
+        )
+
+        if (result.upsertedCount > 0 || result.modifiedCount > 0) {
+            await db.collection('stat').updateOne(
+                { _id: new ObjectId(body.user_id) },
+                {
+                    $inc: { review_count: 1 }, 
+                    $set: { updated_at: new Date(Date.now() + 9 * 60 * 60 * 1000) },
+                }
+            )
+        }
+
+        return NextResponse.json({ message: "리뷰 작성 성공" }, { status: 200 })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ message: "실패" }, { status: 500 })
+    }
+
+}
---
+++ app/write/review/page.tsx
@@ -0,0 +1,105 @@
+'use client'
+
+import { useState } from 'react';
+import LeftMenu from '../../library/LeftMenu';
+import styles from './WriteReview.module.css'
+import { PiStarFill } from "react-icons/pi";
+import { useSearchParams } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
+export default function WriteReview() {
+
+    const { data: session } = useSession()
+
+    const params = useSearchParams()
+
+    const cover = params.get('cover') || ''
+    const title = params.get('title') || ''
+    const author = params.get('author') || ''
+    const isbn = params.get('isbn13')
+
+    const [rating, setRating] = useState(0)
+    const [content, setContent] = useState('')
+
+    const handleSubmit = async () => {
+        if (!content) {
+            alert('리뷰 내용을 작성해주세요.')
+            return
+        }
+
+        if (rating === 0) {
+            alert('별점을 평가해주세요.')
+            return
+        }
+
+        const data = {
+            isbn,
+            title,
+            cover,
+            author,
+            content,
+            rating,
+            sub: session?.user.sub
+        }
+
+        try {
+            const response = await fetch('/api/db/review/write', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+
+            const result = await response.json()
+
+            if (response.status === 200) {
+                alert('리뷰 작성 성공')
+            } else {
+                alert(result.message || '리뷰 작성 실패')
+            }
+        } catch (error) {
+            alert('리뷰 작성 중 오류가 발생했습니다.')
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <h2 className={styles.menuTitle}>리뷰 쓰기</h2>
+                <div className={styles.line}></div>
+                <div className={styles.bookInfo}>   
+                    <img className={styles.cover} src={cover} alt={title} />
+                    <div className={styles.bookDetail}>
+                        <span className={styles.title}>{title}</span>
+                        <span className={styles.author}>{author}</span>
+
+                        <div className={styles.rating}>
+                            {
+                                [...Array(rating)].map((a,i) => (
+                                    <PiStarFill className={styles.starFill} key={i} onClick={() => setRating(i + 1)} />
+                                ))
+                            }
+
+                            {
+                                [...Array(5 - rating)].map((a,i) => (
+                                    <PiStarFill className={styles.star} key={i} onClick={() => setRating(rating + i + 1)}/>
+                                ))
+                            }
+                        </div>
+
+                    </div>
+                </div>
+                <textarea 
+                    className={styles.review}
+                    maxLength={1000}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+                <div className={styles.btnBox}>
+                    <button onClick={handleSubmit} className={styles.btn}>등록</button>
+                </div>
+            </div>
+        </div>
+    )
+}
