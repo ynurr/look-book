7078725548b5076f2ca291f0155f317cb3440cff
ynurr <0305_08@naver.com>
2025-01-19 05:52:54 +0900

    fix: 나의 리뷰 조회 Redux로 변경

--- app/library/my-review/page.tsx
+++ app/library/my-review/page.tsx
@@ -9,53 +9,27 @@ import { useEffect, useState } from "react";
 import { LuThumbsUp } from "react-icons/lu";
 import { RiThumbUpFill } from "react-icons/ri";
 import { FaRegCommentDots } from "react-icons/fa";
-
-interface Review {
-    title: string;
-    cover: string;
-    rating: number;
-    content: string;
-    like_count: number;
-    created_at: string;
-}
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store/store";
+import { fetchReviewAll } from "@/store/slices/reviewSlice";
 
 export default function Review() {
 
-    const { data: session, status } = useSession()
+    const { data: session, status } = useSession();
 
     if (!session && status !== "loading") {
         redirect('/login');
     }
 
-    const [reviews, setReviews] = useState<Review[]>([])
+    const dispatch = useDispatch<AppDispatch>();
+    const reviews = useSelector((state: RootState) => state.review.reviews || []);
 
     useEffect(() => {
-        if (status === 'authenticated' && session?.user.sub) {
-            const fetchReviews = async() => {
-                try {
-                    const response = await fetch('/api/db/review/list', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({ sub: session?.user.sub })
-                    })
-                    
-                    const result = await response.json()
-
-                    if (response.status === 200) {
-                        setReviews(result)
-                    } else {
-                        alert(result.message)
-                    }
-                } catch (error) {
-                    console.error("리뷰 조회 실패", error);
-                }
-            }
-
-            fetchReviews()
+        if (status === "authenticated" && session?.user.sub) {
+            dispatch(fetchReviewAll(session?.user.sub || ''))
         }
-    }, [session, status])
+    }, [session, dispatch])
 
     return (
         <div className={styles.container}>
@@ -68,30 +42,32 @@ export default function Review() {
                 {reviews.length === 0 ?
                     <p className={styles.noData}>작성된 리뷰가 없습니다.</p>
                     :
-                    reviews.map((review, i) => (
+                    reviews.map((item, i) => (
                         <div className={styles.list} key={i}>
-                            <div className={styles.bookInfo}>
-                                <img className={styles.cover} src={review.cover} alt={review.title}/>
-                                <div className={styles.bookDetail}>
-                                    <span className={styles.title}>{review.title}</span>
-                                    <span className={styles.date}>{review.created_at}</span>
-                                    <div className={styles.rating}>
-                                        {[...Array(5)].map((_, index) => (
-                                            <PiStarFill 
-                                                key={index}
-                                                className={index < review.rating ? styles.starFill : styles.star}
-                                            />
-                                        ))}
+                            <Link href={`/library/reading/detail?id=${session?.user.sub}&isbn=${item.isbn}`}>
+                                <div className={styles.bookInfo}>
+                                    <img className={styles.cover} src={item.cover} alt={item.title}/>
+                                    <div className={styles.bookDetail}>
+                                        <span className={styles.title}>{item.title}</span>
+                                        <span className={styles.date}>{item.created_at}</span>
+                                        <div className={styles.rating}>
+                                            {[...Array(5)].map((_, index) => (
+                                                <PiStarFill 
+                                                    key={index}
+                                                    className={index < item.rating ? styles.starFill : styles.star}
+                                                />
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className={styles.reviewBox}>
-                                <span className={styles.review}>{review.content}</span>
-                            </div>
-                            <div className={styles.reactionBox}>
-                                <span className={styles.likeCnt}><LuThumbsUp /> 0{review.like_count}</span>
-                                <span className={styles.commentCnt}><FaRegCommentDots /> 0</span>
-                            </div>
+                                <div className={styles.reviewBox}>
+                                    <span className={styles.review}>{item.content}</span>
+                                </div>
+                                <div className={styles.reactionBox}>
+                                    <span className={styles.likeCnt}><LuThumbsUp />{item.like_count}</span>
+                                    <span className={styles.commentCnt}><FaRegCommentDots /> 0</span>
+                                </div>
+                            </Link>
                             <div className={styles.hrLine}></div>
                         </div>
                     ))
