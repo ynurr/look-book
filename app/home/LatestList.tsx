f78ecd5e03cc87e9ccd93d6674dabb78961b5b5d
ynurr <0305_08@naver.com>
2025-02-06 05:17:13 +0900

    refactor: 기존 슬라이더 -> react-horizontal-scrolling-menu 변경

--- app/home/LatestList.tsx
+++ app/home/LatestList.tsx
@@ -1,87 +1,97 @@
 'use client'
 
-import React, { useEffect } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons'
-import Slider from 'react-slick';
+import React, { useContext, useEffect } from 'react'
 import styles from './../(styles)/Common.module.css'
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, AppDispatch } from '@/store/store';
 import { fetchBookList, Books } from '@/store/slices/listSlice';
 import { IoChevronForward } from 'react-icons/io5';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+interface BookItem {
+    isbn13: string;
+    title: string;
+    author: string;
+    cover: string;
+}
 
 export default function LatestList() {
+
+    const MenuItem: React.FC<{ book: BookItem }> = ({ book }) => {
+        return (
+            <div className={styles.item}>
+                <Link href={`/detail?id=${book.isbn13}`} legacyBehavior>
+                    <img className={styles.cover} src={book.cover} alt={book.title} />
+                </Link>
+                <Link href={`/detail?id=${book.isbn13}`} className={styles.title} >{book.title}</Link>
+                <p className={styles.author}>{book.author}</p>
+            </div>
+        )
+    };
+    
+    type VisibilityType = React.ContextType<typeof VisibilityContext>;
+    
+    const LeftArrow: React.FC = () => {
+        const visibility = useContext<VisibilityType>(VisibilityContext);
+        const isFirstItemVisible = visibility.useIsVisible('first', true);
+        return (
+            <button
+                disabled={isFirstItemVisible}
+                onClick={() => visibility.scrollPrev()}
+                className={styles.leftArrow}
+            >
+            <FaChevronLeft
+                size={10}
+                className={`${styles.leftBnt} ${isFirstItemVisible ? styles.disabledBtn : styles.activeBtn}`}
+            />
+            </button>
+        );
+    };
+    
+    const RightArrow: React.FC = () => {
+        const visibility = useContext<VisibilityType>(VisibilityContext);
+        const isLastItemVisible = visibility.useIsVisible('last', false);
+        return (
+            <button
+                disabled={isLastItemVisible}
+                onClick={() => visibility.scrollNext()}
+                className={styles.rightArrow}
+            >
+            <FaChevronRight
+                size={10}
+                className={`${styles.rightBnt} ${isLastItemVisible ? styles.disabledBtn : styles.activeBtn}`}
+            />
+            </button>
+        );
+    };
+
     const dispatch = useDispatch<AppDispatch>();
     const books = useSelector((state: RootState) => state.list.latestList);
 
     useEffect(() => {
         dispatch(fetchBookList({ type: 'ItemNewSpecial', max: '24', page: '1' }));
-    }, [])
-
-    const PrevArrow = (props: any) => {
-        const { onClick, currentSlide } = props;
-        return (
-            <FontAwesomeIcon icon={faAngleLeft} className={`prev-button ${currentSlide === 0 ? 'hidden' : ''}`} onClick={onClick} />
-        );
-    };
-
-    const NextArrow = (props: any) => {
-        const { onClick, currentSlide } = props;
-        const isLastSlide = currentSlide >= books.length - 6;
-        return (
-            <FontAwesomeIcon icon={faAngleRight} className={`next-button ${isLastSlide ? 'hidden' : ''}`} onClick={onClick} />
-        );
-    };
-
-    const settings = {
-        dots: false,
-        arrows: true,
-        draggable: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        prevArrow: <PrevArrow />,
-        nextArrow: <NextArrow />,
-        responsive: [
-            {
-                breakpoint: 1023,
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 6,
-                    arrows: false,
-                    draggable: true
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
-                    arrows: false,
-                    draggable: true
-                }
-            }
-        ]
-    };
+    }, [dispatch]);
 
     return (
         <div className='slider-wrapper latest-slider'>
-            <Link href="/new" className={styles.categoryLink}>화제의 신간<IoChevronForward /></Link>
-            <Slider {...settings}>
-                {books.map((book: Books, index: number) => (
-                    <div key={index} className='slider-item'>
-                        <Link href={`/detail?id=${book.isbn13}`}>
-                            <img className={styles.cover} src={book.cover} alt={book.title} />
-                        </Link>
-                        <Link href={`/detail?id=${book.isbn13}`}>
-                            <span className={styles.title}>{book.title}</span>
-                        </Link>
-                        <p className={styles.author}>{book.author}</p>
-                    </div>
-                ))}
-            </Slider>
+            <Link 
+                href="/new" 
+                className={styles.categoryLink}
+            >화제의 신간<IoChevronForward />
+            </Link>
+            <div className={styles.scrollWrapper}>
+                <ScrollMenu
+                    scrollContainerClassName={styles.scrollContainer}
+                    LeftArrow={LeftArrow}
+                    RightArrow={RightArrow}
+                >
+                    {books.map((book: Books, index: number) => (
+                        <MenuItem book={book} key={index} />
+                    ))}
+                </ScrollMenu>
+            </div>
         </div>
     );
 }
