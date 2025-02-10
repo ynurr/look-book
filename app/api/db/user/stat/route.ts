9a744ffce2ab022bf8ec09269fa82590c9f5a3ff
ynurr <0305_08@naver.com>
2025-02-11 02:55:47 +0900

    feat: goal(독서목표) 추가

--- app/api/db/user/stat/route.ts
+++ app/api/db/user/stat/route.ts
@@ -4,28 +4,29 @@ import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    const body = await req.json()
+    const body = await req.json();
 
-    if (!body.sub) {
-        return NextResponse.json({ message: "유효하지 않은 사용자 ID입니다." }, { status: 400 })
+    if (!body.user_id) {
+        return NextResponse.json({ message: "유효하지 않은 사용자 ID입니다." }, { status: 400 });
     }
 
     try {
-        const db = (await connectDB).db("lookbook")
+        const db = (await connectDB).db("lookbook");
         
-        const userStat = await db.collection("stat").findOne({ user_id: new ObjectId(body.sub) });
+        const userStat = await db.collection("stat").findOne({ user_id: new ObjectId(body.user_id) });
 
         if (!userStat) {
             return NextResponse.json({ message: "데이터가 존재하지 않습니다." }, { status: 404 });
         }
 
-        const lastRead = userStat.updated_at? differenceInDays(new Date(), new Date(userStat.updated_at)) : "-"
+        const lastRead = userStat.updated_at? differenceInDays(new Date(), new Date(userStat.updated_at)) : "-";
         
         return NextResponse.json({
+            goal: userStat.goal,
             bookCount: userStat.book_count,
             reviewCount: userStat.review_count,
             lastRead: lastRead
-        }, { status: 200 })
+        }, { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json({ message: "서버 오류가 발생했습니다." }, { status: 500 });
