e557aca16627a9ca20f82177ee37b69b0d6a59a4
ynurr <0305_08@naver.com>
2025-03-19 19:04:31 +0900

    fix: 리스트 key 값 index로 변경하여 중복 문제 해결

--- app/(components)/List.tsx
+++ app/(components)/List.tsx
@@ -16,10 +16,10 @@ interface ListProps {
 
 export default function List({items}: ListProps) {
 
-    const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태 관리
+    const [currentPage, setCurrentPage] = useState(1);
     const ItemsPerPage = 24; 
 
-    const pageCount = Math.ceil(items.length / ItemsPerPage); // 총 페이지 수
+    const pageCount = Math.ceil(items.length / ItemsPerPage);
     const currentItems = items.slice((currentPage - 1) * ItemsPerPage, currentPage * ItemsPerPage);
 
     const handlePageChange = (selected: { selected: number }) => {
@@ -31,13 +31,12 @@ export default function List({items}: ListProps) {
         <div className={styles.container}>
             <div className={styles.contents}>
                 {currentItems.map((item, index) => (
-                    <div className={styles.item} key={item.isbn13}>
+                    <div className={styles.item} key={index}>
                         <Link href={`/detail?id=${item.isbn13}`}>
                             <img className={styles.cover} src={item.cover} alt={item.title}></img>
                             <p className={styles.title}>{item.title}</p>
                             <p className={styles.author}>{item.author}</p>
                         </Link>
-                        {/* <div className={styles.rating}>별점</div> */}
                     </div>
                 ))}
                 <Pagination
