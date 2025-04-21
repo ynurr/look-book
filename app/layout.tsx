4ce5f9badbdeaf3310eefa84ec3b9ede6b1e3e8d
ynurr <0305_08@naver.com>
2025-04-22 01:36:54 +0900

    chore: 사용하지 않는 라이브러리 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,6 @@ import Footer from "./components/Footer";
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 import localFont from "next/font/local";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import NavbarManager from "./components/NavbarManager";
 
 
